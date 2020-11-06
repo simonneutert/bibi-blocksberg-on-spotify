51176c91b0f6c297331c1d4c65e075ef5b7e77da
Simon Neutert <simon@Jerohimomo.fritz.box>
2020-11-06 19:04:55 +0100

    search as component

---
+++ components/TheSearchForm.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div id="search-form">
+      <b-input
+        v-model="searchInput"
+        type="text"
+        placeholder="Suche nach Namen einer Episode"
+        @keyup="searchEpisode()"
+      />
+    </div>
+
+    <div id="episodes">
+      <div v-if="searchableItems.length">
+        <div v-for="album in searchableItemsCopy" :key="album.uri">
+          <div class="row py-4">
+            <div class="col-sm-2">
+              <b-img :src="album.images[1].url" fluid />
+            </div>
+            <div class="col-sm-10">
+              <h4>{{ album.name }}</h4>
+              <p>
+                Episode {{ album.episode }}
+                <a :href="album.href">(auf Spotify Website)</a>
+              </p>
+              <div class="play">
+                <p>
+                  <a :href="album.spotifyUri">Jetzt auf Spotify abspielen</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="m-4 p-4">
+        <h4>Keine Treffer</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchInput: '',
+      searchableItemsCopy: []
+    }
+  },
+  props: {
+    searchableItems: { type: Array, required: true }
+  },
+  methods: {
+    searchEpisode() {
+      console.log(this.searchableItems)
+      if (this.searchInput.length) {
+        this.searchableItemsCopy = this.searchableItems.slice()
+        const result = this.searchableItemsCopy.filter(
+          function(album) {
+            return (
+              album.name
+                .toLowerCase()
+                .includes(this.searchInput.toLowerCase()) ||
+              String(album.episode).includes(this.searchInput)
+            )
+          }.bind(this)
+        )
+
+        this.searchableItemsCopy = result
+      } else {
+        this.restorePlaylist()
+      }
+    },
+    restorePlaylist() {
+      this.searchableItemsCopy = this.searchableItems.slice()
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,7 @@
 <template>
   <b-container>
     <ScrollToBottom />
-    <div id="search-form">
-      <b-input
-        v-model="searchInput"
-        type="text"
-        placeholder="Suche nach Namen einer Episode"
-        @keyup="searchEpisode()"
-      />
-    </div>
-
-    <div v-for="album in playlist.items" :key="album.uri">
-      <div class="row py-4">
-        <div class="col-sm-2">
-          <b-img :src="album.images[1].url" fluid />
-        </div>
-        <div class="col-sm-10">
-          <h4>{{ album.name }}</h4>
-          <p>
-            Episode {{ album.episode }}
-            <a :href="album.href">(auf Spotify Website)</a>
-          </p>
-          <div class="play">
-            <p><a :href="album.spotifyUri">Jetzt auf Spotify abspielen</a></p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <TheSearchForm :searchableItems="playlist.items" />
   </b-container>
 </template>
 
@@ -34,43 +9,17 @@
 import playlist from '@@/static/bibi_formatted.json'
 
 export default {
-  components: {},
   data() {
     return {
       playlist,
-      playlistInit: playlist,
-      searchInput: ''
-    }
-  },
-  methods: {
-    searchEpisode() {
-      if (this.searchInput.length) {
-        this.playlist = Object.assign({}, this.playlistInit)
-        const result = this.playlist.items.filter(
-          function(album) {
-            return (
-              album.name
-                .toLowerCase()
-                .includes(this.searchInput.toLowerCase()) ||
-              String(album.episode).includes(this.searchInput)
-            )
-          }.bind(this)
-        )
-
-        if (result.length) {
-          this.playlist.items = result
-        } else {
-          this.restorePlaylist()
-        }
-      } else {
-        this.restorePlaylist()
-      }
-    },
-    restorePlaylist() {
-      this.playlist = Object.assign({}, this.playlistInit)
+      playlistInit: playlist
     }
   }
 }
 </script>
 
-<style></style>
+<style>
+#episodes {
+  min-height: 70vh;
+}
+</style>
