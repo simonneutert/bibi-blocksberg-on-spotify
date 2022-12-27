1e9552ae44b3500aaf2113cd73eb4aa41dd8395c
Simon Neutert <simonneutert@users.noreply.github.com>
2022-12-27 19:25:14 +0100

    nextjs

---
+++ README.md
@@ -0,0 +1,54 @@
+# Bibi Blocksberg on Spotify
+
+a simple NextJS app
+## get the source data
+
+- make dev account for api access on spotify
+- 2YlvvdXUqRjiXmeL2GRuZ9 is "Holmes's Spotify Artist ID"
+- visit https://developer.spotify.com/console/get-artist-albums/ then scroll around until you find the "try it" button, it will generate a key, which in turn allows you to curl what you need (pipe to file :wink:)
+
+## extract json using JSONata
+
+``` jsonata
+{
+    "items": $sort(items[$contains(name, 'Die Originale')].{
+        "name": name,
+        "uri": uri,
+        "images": images,
+        "spotifyUrl": external_urls.spotify,
+        "case": $number($split($match(name, /[Folge|Fall] \d+/).match, " ")[-1])
+    }, function($l, $r) {
+        $l.case > $r.case
+    }
+    )
+}
+```
+
+## Getting Started
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+
+[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+
+The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import Head from 'next/head'
+import Albums from '../components/albums'
+
+import bibi from '../public/bibi_formatted.json'
+
+import { useEffect, useState } from 'react'
+
+export default function Home() {
+  const shuffle = {
+    items: [
+      bibi.items[Math.floor(Math.random() * bibi.items.length)]
+    ]
+  }
+
+  const [albums, setAlbums] = useState([])
+  useEffect(() => setAlbums(shuffle), [])
+
+  const [albumsFiltered, setAlbumsFiltered] = useState([])
+  useEffect(() => setAlbumsFiltered(bibi), [])
+
+  const [searchFilter, setSearchFilter] = useState("")
+  // useEffect(() => setSearchFilter(searchFilter), "")
+
+  function watchInput(event) {
+    const newInputValue = event.target.value
+    setSearchFilter(newInputValue)
+    if (newInputValue.length >= 3) {
+      const hits = bibi.items.filter((item) => {
+        return item.name.toLowerCase().match(newInputValue.toLowerCase())
+      })
+      setAlbumsFiltered({ items: hits })
+    } else {
+      setAlbumsFiltered(bibi)
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Bibi Blocksberg on Spotify</title>
+        <meta name="description" content="Browse Bibi Blocksberg Hörspiele auf Spotify" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        <h2>Finde immer das passende Hörspiel von Bibi Blocksberg auf Spotify</h2>
+        <input type="text" value={searchFilter} onChange={watchInput} placeholder="Suche ..." />
+      </div>
+
+      {[albums, albumsFiltered].map((coll, i) => {
+        if (i == 0) {
+          return <div className='container' key={i}>
+            <div class="d-flex justify-content-center">
+              <div>
+                <div className='my-4'>
+                  <h4>Zufälliges Album</h4>
+                  <Albums albums={coll} cardMaxWidth="100%" />
+                </div>
+              </div>
+            </div>
+          </div>
+        } else {
+          return <div className='container' key={i} >
+            <h2>Alle Hörspiele von Bibi Blocksberg auf Spotify</h2>
+            <div className='my-4'>
+              <Albums albums={coll} />
+            </div>
+          </div>
+        }
+      })}
+    </>
+  )
+}
