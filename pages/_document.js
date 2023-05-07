84798c77405c64a3bf66c144b1a2ba6115714632
Simon Neutert <simonneutert@users.noreply.github.com>
2023-05-07 11:00:01 +0200

    drops bootstrap cdn

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import Nav from '../components/nav'
-import Footer from '../components/footer'
+import { Html, Head, Main, NextScript } from "next/document";
+import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" defer />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous" defer></script>
+        <link href="/bootstrap.min.css" rel="stylesheet" defer />
+        <script src="/bootstrap.bundle.min.js" defer></script>
       </Head>
 
       <body>
-
         <Nav />
 
         <div className="container my-4">
@@ -23,5 +22,5 @@ export default function Document() {
         <Footer />
       </body>
     </Html>
-  )
+  );
 }
